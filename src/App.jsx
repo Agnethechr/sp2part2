4227153ef1eae4b91776b2f97004b12ae406abd6
Agnethechr <agnethechr18@gmail.com>
2025-05-20 14:51:42 +0200

    opdateret quiz page og stylet, tilføjet favicon og ændret titlen

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 import MainPage from './RouteComponents/MainPage';
 import VisionPage from './RouteComponents/VisionPage';
 import EndpointsPage from './RouteComponents/EndpointsPage';
+import QuizPage from './RouteComponents/QuizPage';
 
 function App() {
   return (
@@ -18,13 +19,15 @@ function App() {
       <nav>
         <Link to="/">Main</Link> |{" "}
         <Link to="/vision">Vision</Link> |{" "}
-        <Link to="/endpoints">Endpoints</Link>
+        <Link to="/endpoints">Endpoints</Link> |{" "}
+        <Link to="/quiz">Quiz</Link> |{" "}
       </nav>
 
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/vision" element={<VisionPage />} />
         <Route path="/endpoints" element={<EndpointsPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
       </Routes>
     </Router>
   );
